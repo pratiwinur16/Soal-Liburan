8e313b7b79d004715605fb58ebb647884ee1a4d9
pratiwi <pratiwinur.amini@gmail.com>
2018-06-19 12:00:45 +0700

    Soal objek

--- 09Juni2018/002.js
+++ 09Juni2018/002.js
@@ -1,8 +1,17 @@
-function soal2(param)
-{
-    // your code here 
-
+function soal2 (param){
+    for (var i = 0; i<param.length ; i++ ){
+      var id = 
+      {
+        firstName: param[i][0],
+        lastname: param[i][1],
+        hobbies: param [i][2],
+        profeesion: param[i][3],
+      }
+    } return (id)
 }
+  
+  console.log([ ['Awtian','Akbar',['coding','main kucing','anime'],'Web Instructur'] , 
+  ['Megalo','Maniak',['metal music','maniac'],'Maniac Expert'] ]);
 
 console.log([ ['Aries','Dimas',['coding','pubg','anime'],'Web Developer'] , 
 ['Radit','Dika',['manga','gaming'],'Web Developer'] ]);
