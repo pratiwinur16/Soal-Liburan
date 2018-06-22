19c4b8d5b1aca365e8b981651c7e289cd019f0f2
pratiwinur16 <39262584+pratiwinur16@users.noreply.github.com>
2018-06-22 13:34:18 +0700

    Update 003.js

--- 13Juni2018/003.js
+++ 13Juni2018/003.js
@@ -1,8 +1,19 @@
-// hanya boleh menggunakan rekursif
 
+
+// hanya boleh menggunakan rekursif
+var res=1;
 function soal3(param1, param2)
 {
-    // your code here 
+  //return Math.pow(param1,param2)
+    res=res*param1;
+    param2--;
+   if (param2 === 0){
+     var temp=res;
+     res=1;
+     return temp;
+     }
+   else {
+  return soal3 (param1,param2 )}
 }
 
 console.log(soal3(2,5))
@@ -15,3 +26,5 @@ console.log(soal3(3,3))
 /*
     27
 */
+console.log(soal3(5,6))
+
