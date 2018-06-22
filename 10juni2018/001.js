54f31ab50e1362424f9c76429f6964348fcf46e8
pratiwinur16 <39262584+pratiwinur16@users.noreply.github.com>
2018-06-22 13:32:12 +0700

    Update 001.js

--- 10juni2018/001.js
+++ 10juni2018/001.js
@@ -2,7 +2,33 @@
     analisa sendiri dengan apa yang diminta soal melalui test case. 
     wajib menggunakan pseudocode
     PSEUDOCODE HERE : 
-*/
+     /*FUNCTION soal1
+      parameter:
+      - param 
+    START
+      IF param EQUALS TO 0 
+        RETURN INVALID INPUT
+      STORE Result with empty array 
+      Sotre exclamationmark with "!"
+      sotre ad with '@'
+      sotre hastag with '#'
+      sotre counter with 0 
+      FOR `i` to length of param
+        if counter equals to 0 then 
+          add `exclamationmark` index of 0 to `result`
+          increment counter 
+        if  counter equals to 1 then 
+          add `ad` index of 1 to `result`
+          increment counter 
+         if  counter equals to 1 then 
+          add `hastag` index of 2 to `result`
+          counter set to 0 
+        ENDIF 
+      ENDFOR 
+      RETURN result 
+    ENd function 
+*/   
+
 function soal1(param)
 {
   if (param  === 0 ){
@@ -12,13 +38,17 @@ function soal1(param)
     var exclamationMark = '!'
     var ad = "@";
     var hashtag = "#"
+    var counter=0;
      for (var i=0; i<=param; i++){
-       if (result.length===0){
+       if (counter===0){
          result.push (exclamationMark)
-       } else if (result.length===1) {
+         counter++
+       } else if (counter===1) {
          result.push (ad) 
-       } else if (result.length===3 ){
+         counter++
+       } else if (counter===2 ){
          result.push (hashtag)
+         counter = 0
        }
      }
     return result 
@@ -34,3 +64,5 @@ console.log(soal1(4))
 // output : ['!','@','#','!']
 
 console.log(soal1(0)) // invalid input
+*/
+
